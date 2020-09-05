b5f93d9b11b95b5dcddc99a9037f3dcf999456f9
Samuel Porto <samuelportodossantos@gmail.com>
2020-09-05 17:19:22 -0300

    Implementa menu com reset game, reset score, e resume

--- Assets/Sprites/menu.cs
+++ Assets/Sprites/menu.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class menu : MonoBehaviour
+public class Menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public GameObject menu;
+    public Text bestScoreText;
+
+    public void open()
     {
-        
+        Time.timeScale = 0;
+        this.menu.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void close()
     {
-        
+        Time.timeScale = 1;
+        this.menu.SetActive(false);
     }
+
+    public void resetBestScore()
+    {
+        if ( PlayerPrefs.HasKey("score"))
+        {
+            print("resetando best score!");
+            PlayerPrefs.SetInt("score", 0);
+            bestScoreText.text = "BEST SCORE: 0";
+        }
+    }
+
+    public void resetGame()
+    {
+        this.close();
+        SceneManager.LoadScene("game");
+    }
+
 }
